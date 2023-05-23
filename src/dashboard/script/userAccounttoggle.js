510eaad722911ddc6b78d883d6a5787aecb8e71f
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2023-05-23 02:48:59 +0000

    login checking is successfull

--- src/dashboard/script/userAccounttoggle.js
+++ src/dashboard/script/userAccounttoggle.js
@@ -1,21 +1,23 @@
-if(localStorage.getItem('SignUpData') == null)
-        body.innerHTML = '502 Bad Getway';
-
-let UserName = document.getElementById('UserName'),
-UserNameWel = document.getElementById('UserWelcome');
-let data = localStorage.getItem('SignUpData');
-let UserData = JSON.parse(data);
-
-UserName.innerText = UserData.userName;
-UserNameWel.innerText = `Welcome, ${UserData.userName}`;
-
-let AccountBtn = document.querySelector('.UserAccount');
-let AccountBox = document.getElementById('UserAccountBox')
-AccountBtn.addEventListener('click', () => {
-    AccountBox.classList.toggle('UserAccountActive');
-    AccountBox.classList.toggle('UserAccounthidden');
-})
-let container = document.querySelector('.container').addEventListener('click', () => {
-    AccountBox.classList.add('UserAccounthidden');
-    AccountBox.classList.remove('UserAccountActive');
-})
+if(localStorage.getItem('SignUpData') == null){
+    document.body.innerHTML = `<h1 style="text-align:center;">502 Bad Getway</h1>`;
+}
+else {
+    let UserName = document.getElementById('UserName'),
+    UserNameWel = document.getElementById('UserWelcome');
+    let data = localStorage.getItem('SignUpData');
+    let UserData = JSON.parse(data);
+    
+    UserName.innerText = UserData.userName;
+    UserNameWel.innerText = `Welcome, ${UserData.userName}`;
+    
+    let AccountBtn = document.querySelector('.UserAccount');
+    let AccountBox = document.getElementById('UserAccountBox')
+    AccountBtn.addEventListener('click', () => {
+        AccountBox.classList.toggle('UserAccountActive');
+        AccountBox.classList.toggle('UserAccounthidden');
+    })
+    let container = document.querySelector('.container').addEventListener('click', () => {
+        AccountBox.classList.add('UserAccounthidden');
+        AccountBox.classList.remove('UserAccountActive');
+    })
+}
