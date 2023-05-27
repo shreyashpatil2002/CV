f501090473f60e7682b1b22bb349d4fe03de76b3
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2023-05-27 03:45:32 +0000

    Alpha next btn of all resume forms are working

--- src/dashboard/script/savingResumeData.js
+++ src/dashboard/script/savingResumeData.js
@@ -12,31 +12,32 @@ const resumeForm1Submitted = () => {
   dataFields.push(textarea);
 
   form1SubmitBtn.addEventListener("click", () => {
-      errorField.innerHTML = "";
-      let errorCount = 0;
-      dataFields.forEach((element) => {
-        if (element.value == "") {
-          errorField.innerHTML += `<li>* ${element.attributes[3].nodeValue} is Required </li>`;
-          element.style.border = "1px solid red";
-          errorCount++;
-          return false;
-        } else {
-          element.style.border = "1px solid #767676";
-        }
-      });
-      if (errorCount == 0) {
-        const personalInfo = {
-          firstName: dataFields[0].value,
-          lastName: dataFields[1].value,
-          jobRole: dataFields[2].value,
-          jobEmail: dataFields[3].value,
-          phone: dataFields[4].value,
-          userAddress: dataFields[5].value,
-          aboutUser: dataFields[6].value,
-        };
-        console.log(personalInfo);
-        const personalInfostring = JSON.stringify(personalInfo);
-        localStorage.setItem("personalInfo", personalInfostring);
+    errorField.innerHTML = "";
+    let errorCount = 0;
+    dataFields.forEach((element) => {
+      if (element.value == "") {
+        errorField.innerHTML += `<li>* ${element.attributes[3].nodeValue} is Required </li>`;
+        element.style.border = "1px solid red";
+        errorCount++;
+        return false;
+      } else {
+        element.style.border = "1px solid #767676";
+      }
+    });
+    if (errorCount == 0) {
+      const personalInfo = {
+        firstName: dataFields[0].value,
+        lastName: dataFields[1].value,
+        jobRole: dataFields[2].value,
+        jobEmail: dataFields[3].value,
+        phone: dataFields[4].value,
+        userAddress: dataFields[5].value,
+        aboutUser: dataFields[6].value,
+      };
+      console.log(personalInfo);
+      const personalInfostring = JSON.stringify(personalInfo);
+      localStorage.setItem("personalInfo", personalInfostring);
+      resumeForm2layout();
     }
   });
 };
@@ -71,6 +72,36 @@ const resumeForm2Submitted = () => {
       console.log(educationInfo);
       const educationInfostring = JSON.stringify(educationInfo);
       localStorage.setItem("educationInfo", educationInfostring);
+      resumeForm3layout();
     }
   });
 };
+
+const resumeForm3Submitted = () => {
+  AddedExp();
+  let form1SubmitBtn = document.getElementById("Form3btn");
+  form1SubmitBtn.addEventListener("click", () => {
+    AddedExp();
+    resumeForm4layout();
+  });
+};
+
+let expCount = 0;
+function AddedExp() {
+  let dataFields = document.querySelectorAll("input");
+  let textarea = document.querySelector("textarea");
+  dataFields = Array.from(dataFields);
+  dataFields.pop();
+  dataFields.pop();
+  dataFields.push(textarea);
+  if (dataFields[0].value != "") {
+    const workExpInfo = {
+      companyName: dataFields[0].value,
+      companyJobRole: dataFields[1].value,
+      companyExp: dataFields[2].value,
+    };
+    console.log(workExpInfo);
+    const workExpInfostring = JSON.stringify(workExpInfo);
+    localStorage.setItem(`workExpInfo${expCount++}`, workExpInfostring);
+  }
+}
