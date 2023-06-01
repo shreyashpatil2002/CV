5648011548d53d22b5fdcd7db75ed8d61c035508
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2023-06-01 10:49:31 +0000

    toggle chages are made

--- src/dashboard/script/userAccounttoggle.js
+++ src/dashboard/script/userAccounttoggle.js
@@ -1,25 +1,28 @@
-if(localStorage.getItem('SignUpData') == null){
-    document.body.innerHTML = `<h1 style="text-align:center;">502 Bad Getway</h1>`;
-}
-else {
-    let title = document.querySelector('title');
-    let UserName = document.getElementById('UserName'),
-    UserNameWel = document.getElementById('UserWelcome');
-    let data = localStorage.getItem('SignUpData');
-    let UserData = JSON.parse(data);
-    
-    title.innerText = `${UserData.userName}@CV - Dashboard`;
-    UserName.innerText = UserData.userName;
-    UserNameWel.innerText = `Welcome, ${UserData.userName}`;
-    
-    let AccountBtn = document.querySelector('.UserAccount');
-    let AccountBox = document.getElementById('UserAccountBox')
-    AccountBtn.addEventListener('click', () => {
-        AccountBox.classList.toggle('UserAccountActive');
-        AccountBox.classList.toggle('UserAccounthidden');
-    })
-    let container = document.querySelector('.container').addEventListener('click', () => {
-        AccountBox.classList.add('UserAccounthidden');
-        AccountBox.classList.remove('UserAccountActive');
-    })
+if (localStorage.getItem("SignUpData") == null) {
+  document.body.innerHTML = `<h1 style="text-align:center;">502 Bad Getway</h1>`;
+} else {
+  let title = document.querySelector("title");
+  let UserName = document.getElementById("UserName"),
+    UserNameWel = document.getElementById("UserWelcome");
+  let data = localStorage.getItem("SignUpData");
+  let UserData = JSON.parse(data);
+
+  title.innerText = `${UserData.userName}@CV - Dashboard`;
+  UserName.innerText = UserData.userName;
+  UserNameWel.innerText = `Welcome, ${UserData.userName}`;
+
+  let AccountBtn = document.querySelector(".UserAccount");
+  let AccountBox = document.getElementById("UserAccountBox");
+  let msgContent = document.getElementById("massageInfo");
+  AccountBtn.addEventListener("click", () => {
+    AccountBox.classList.toggle("UserAccountActive");
+    AccountBox.classList.toggle("UserAccounthidden");
+    msgContent.classList.add("inactiveMsg");
+  });
+  let container = document
+    .querySelector(".container")
+    .addEventListener("click", () => {
+      AccountBox.classList.add("UserAccounthidden");
+      AccountBox.classList.remove("UserAccountActive");
+    });
 }
